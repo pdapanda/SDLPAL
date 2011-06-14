1060dbedc75018c2350b9ff73d4c5cd25d2ad581
weimingzhi_cp <SND\weimingzhi_cp>
2011-06-13 23:32:13 -0700

    removed unnecessary includes (Patch #9715)

--- sdlpal/map.c
+++ sdlpal/map.c
@@ -18,10 +18,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL.h>
-
 #include "palcommon.h"
 #include "map.h"
 
